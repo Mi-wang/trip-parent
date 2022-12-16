f6d85d80029019ca5c5cc08afcf617087a51cc29
Mi-Manchi <2046380350@qq.com>
2022-12-16 15:06:36 +0800

    前端文件

---
+++ trip-servers/trip-user-server/src/main/java/cn/wolfcode/mapper/UserInfoMapper.java
@@ -0,0 +1,12 @@
+package cn.wolfcode.mapper;
+
+import cn.wolfcode.domain.UserInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author wby
+ * @version 1.0
+ * @date 2022/12/15 11:40
+ */
+public interface UserInfoMapper extends BaseMapper<UserInfo> {
+}
---
+++ trip-servers/trip-user-server/src/main/java/cn/wolfcode/service/IUserInfoServer.java
@@ -0,0 +1,12 @@
+package cn.wolfcode.service;
+
+import cn.wolfcode.domain.UserInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @author wby
+ * @version 1.0
+ * @date 2022/12/15 11:41
+ */
+public interface IUserInfoServer extends IService<UserInfo> {
+}
